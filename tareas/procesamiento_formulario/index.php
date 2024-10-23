e5f2105aa0a229c447d6242a7d346a0d755c4dc9
SebasTechMad <personaldesebas@gmail.com>
2024-10-23 02:49:18 +0200

    actualizacion 23-10-24

---
+++ tareas/procesamiento_formulario/index.php
@@ -0,0 +1,103 @@
+<?php
+
+    $estatusImagen = "";
+    $errorImagen = "Error al subir la imagen";
+
+    function getName(){
+        return $_POST['nombre'];
+    }
+
+    function getNickname(){
+        return $_POST['alias'];
+    }
+
+    function getAge(){
+        return $_POST['edad'];
+    }
+
+    function getFile(){
+
+    }
+
+    function getArmas(){
+        $contArmas = 0;
+        $resultado = "";
+
+        if(isset($_POST['arma']))
+        {
+            foreach($_POST['arma'] as $item)
+            {
+                ($contArmas == 0) ? $resultado = $item: $resultado .= ", $item";
+                $contArmas++;
+            }
+        }
+
+        if($contArmas ==0){
+            $resultado = "se va a dar con los puños";
+        }
+
+        return $resultado;
+    }
+
+    function getMagia(){
+        return $_POST['magia'];
+    }
+
+
+
+    function getHTML($name, $nickname, $age){
+        return '<!DOCTYPE html>
+        <html lang="es">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>resultado</title>
+                <link rel="stylesheet" href="./index.css">
+            </head>
+
+            <body>
+                <header class="title-header">
+                    <h1>Datos del jugador</h1>
+                </header>
+
+                <div class="content-jugador">
+                    <div class="info-jugador" id="datos">
+                        <b>Nombre: '.$name.'</b>
+                        <b>Alias: '.$nickname.'</b>
+                        <b>Edad: '.$age.'</b>
+                        <b>Armas seleccionadas: '.getArmas().'</b>
+                        <b>¿Prácticas con artes mágicas? '.getMagia().' </b>
+                    </div>
+
+                    <div class="info-jugador">
+                        <!--TODO COMPROBAR SUBIDA IMAGEN-->
+                        <b></b>
+                        <!--TODO INTRODUCIR IMAGEN-->
+
+                        <!--ERROR SUBIDA IMAGEN-->
+                    </div>
+                </div>
+
+
+            </body>
+        </html>';
+    }
+
+
+
+    
+    if($_SERVER['REQUEST_METHOD']=='GET'){
+         include_once 'captura.html';
+    }else{
+
+        $nombre = getName();
+        $nickname = getNickname();
+        $age = getAge();
+        $htmlContent = getHTML($nombre, $nickname, $age); 
+
+        //echo $htmlContent;
+    }
+?>
+
+
+
