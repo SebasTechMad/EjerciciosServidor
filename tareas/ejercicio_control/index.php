3bca4244606819b77d0ace38d0ab9bd69a13877c
SebasTechMad <personaldesebas@gmail.com>
2024-11-11 19:53:41 +0100

    subida ejercicio-control completado

--- tareas/ejercicio_control/index.php
+++ tareas/ejercicio_control/index.php
@@ -1,25 +1,39 @@
 <?php
 session_start();
 
-include_once('app/funciones.php');
 
-if (  !empty( $_GET['login']) && !empty($_GET['clave'])){
-    if ( userOk($_GET['login'],$_GET['clave'])){
-      if ( getUserRol($_GET['login']) == ROL_PROFESOR){
-        $contenido = verNotaTodas($_GET['login']);
-      } else {
-        $contenido = verNotasAlumno($_GET['login']);
-      }
-      include_once ('app/resultado.php');
-    } 
-    // userOK falso
-    else {
-       $contenido = "El número de usuario y la contraseña no son válidos";
-       include_once('app/acceso.php');
-    }
-} else {
-    $contenido = " Introduzca su número de usuario y su contraseña";
-    include_once('app/acceso.php');
+if(!isset($_SESSION['intentos'])){
+  $_SESSION['intentos'] = 1;
 }
+
+if ($_SESSION['intentos']>=5) {
+  include_once('app/maximo.php');
+}else{
+  include_once('app/funciones.php');
+
+  if (  !empty( $_GET['login']) && !empty($_GET['clave'])){
+      if ( userOk($_GET['login'],$_GET['clave'])){
+        if ( getUserRol($_GET['login']) == ROL_PROFESOR){
+          $contenido = verNotaTodas($_GET['login']);
+        } else {
+          $contenido = verNotasAlumno($_GET['login']);
+        }
+        $_SESSION['intentos'] = 0;
+        include_once ('app/resultado.php');
+      } 
+      // userOK falso
+      else {
+            $_SESSION['intentos'] += 1;
+            print_r($_SESSION['intentos']);
+            $contenido = "El número de usuario y la contraseña no son válidos";
+            include_once('app/acceso.php');
+        }
+      }else{
+      $contenido = " Introduzca su número de usuario y su contraseña";
+      include_once('app/acceso.php');
+    }
+}
+
+
 
 
