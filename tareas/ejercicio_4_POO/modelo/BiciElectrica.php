d33424530387529c8dae9b1e554b8c162dc83ef6
SebasTechMad <personaldesebas@gmail.com>
2024-12-07 19:23:28 +0100

    subida ejercicio 4 objetos

---
+++ tareas/ejercicio_4_POO/modelo/BiciElectrica.php
@@ -0,0 +1,36 @@
+<?php
+
+class Bicicleta{
+    private $id; // Identificador de la bicicleta (entero)
+    private $coordx; // Coordenada X (entero)
+    private $coordy; // Coordenada Y (entero)
+    private $bateria; // Carga de la batería en tanto por ciento (entero)
+    private $operativa; // Estado de la bicleta ( true operativa- false no disponible)
+
+    public function __construct()
+    {
+        
+    }
+
+    public function __get($propiedad)
+    {
+        return (isset($this->$propiedad)) ? $this->$propiedad: "la variable $propiedad no existe/no esta definida";
+    }
+
+    public function __set($propiedad, $valor)
+    {
+        (property_exists($this, $propiedad)) ? $this->$propiedad = $valor:null;
+    }
+
+    public function __toString()
+    {
+        return 'id de la bicicleta->'. $this->$id.', bateria->'.$this->$bateria;
+    }
+
+    public function distancia($x, $y)
+    {
+        return sqrt( ($x - $this->$coordx)^2 +  ($y - $this->$coordy)^2);
+    }
+
+}
+?>
